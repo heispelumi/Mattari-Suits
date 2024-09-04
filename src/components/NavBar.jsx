5e97532070738a82ccbf383e5bc8b1b2cff2d3a6
heispelumi <pelumiatoyebi06@gmail.com>
2024-09-04 13:11:02 +0100

    Nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { CiMenuFries } from "react-icons/ci";
@@ -27,17 +28,21 @@ const NavBar = () => {
   }, [nav]);
 
   return (
-    <div className='w-full container overflow:hidden absolute z-10 flex justify-between items-center h-24'>
+    <div className='w-full container overflow-hidden absolute z-20 flex justify-between items-center h-24'>
       {/* Desktop View */}
       <div className='w-full mx-auto p-3 flex justify-between items-center'>
         {/* Brand Logo */}
         <div className='flex py-8'>
-          <div 
-            className='w-[82px] h-[163px] bg-[#262126] flex items-center justify-center'
-            style={{ clipPath: 'polygon(0 0, 100% 0, 100% 75%, 0 100%)' }}
-          >
-            <img className='w-[76px] h-[64px] object-cover' src={heroimage} alt="" />
-          </div>
+        <div
+                  className="w-[82px] h-[163px]  flex items-center justify-center"
+                  style={{ clipPath: "polygon(0 0, 100% 0, 100% 75%, 0 100%)" }}
+                >
+                  <img
+                    className="w-[76px] h-[64px] object-cover"
+                    src={heroimage}
+                    alt=""
+                  />
+                </div>
           {/* Menu Links for Desktop */}
           <div className='hidden ml-[60px] sm:flex items-center lg:text-[15px] md:text-[12px] space-x-6 md:space-x-4 text-white'>
             <Link to="/" spy={true} smooth={true} offset={50} duration={500} className='hover:text-[#F9DEA5]'>Home</Link>
@@ -61,10 +66,10 @@ const NavBar = () => {
       </div>
 
       {/* Overlay */}
-      {nav && <div className="bg-black/80 fixed top-0 left-0 w-full h-screen z-10"></div>}
+      {nav && <div className="bg-black/80 fixed top-0 left-0 w-full h-screen z-20"></div>}
 
       {/* Top-Sliding Navigation for Mobile */}
-      <div className={nav ? 'fixed top-0 left-0 h-screen w-full bg-[#262126] text-white flex flex-col justify-between items-center z-30 duration-300' : 'fixed top-[-100%] left-0 bg-[#262126] h-screen w-full z-10 duration-300'}>
+      <div className={nav ? 'fixed top-0 left-0 h-screen w-full bg-[#262126] text-white flex flex-col justify-between items-center z-30 duration-300' : 'fixed top-[-100%] left-0 bg-[#262126] h-full w-full z-10 duration-300'}>
         <AiOutlineClose onClick={() => setNav(!nav)} className='absolute top-12 right-12 w-[18.3px] h-[18.3px] cursor-pointer' />
 
         {/* Centered Navigation Links */}
@@ -81,7 +86,7 @@ const NavBar = () => {
               </span>
             </button>
             {dropdownOpen && (
-              <div className="absolute -right-14 rounded z-10 mt-2 w-[200px] p-2 ease-in-out duration-300 text-black bg-white">
+              <div className="absolute -right-14 rounded z-30 mt-2 w-[200px] p-2 ease-in-out duration-300 text-black bg-white">
                 <div className="flex justify-center items-center">
                   <Link className="block text-[15px] py-2">Custom fitting</Link>
                 </div>
@@ -97,13 +102,13 @@ const NavBar = () => {
         </div>
           
         {/* Buttons for Mobile */}
-        <div className='flex flex-col  items-center py-7 space-y-6'>
+        <div className='flex flex-col items-center py-7 space-y-6'>
           <button className='text-[20px] px-[100px] py-[7px] border rounded-md border-white'>Sign in</button>
           <button className='bg-[#F9DEA5] text-[20px] text-black px-[100px] py-[10px] border rounded-md'>Sign up</button>
         </div>
 
         {/* Socials at the Bottom */}
-        <div className='flex items-center justify-center space-x-4 mt-auto mb-3'>
+        <div className='flex items-center justify-center space-x-4 mt-auto z-20'>
           <CiFacebook className='w-[24px] h-[24px]' />
           <RiTwitterXFill className='w-[24px] h-[24px]' />
           <FaInstagram className='w-[24px] h-[24px]' />
